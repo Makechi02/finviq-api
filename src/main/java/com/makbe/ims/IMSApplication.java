f849e503a5eb41ffe63883ac541b8aadf0c77ae9
Makechi <makechieric9@gmail.com>
2024-09-25 19:46:09 +0300

    update default Time Zone

--- src/main/java/com/makbe/ims/IMSApplication.java
+++ src/main/java/com/makbe/ims/IMSApplication.java
@@ -5,12 +5,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
+import java.time.ZoneId;
+import java.util.TimeZone;
+
 @SpringBootApplication
 @EnableMongoAuditing
 @EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 public class IMSApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone(ZoneId.of("Africa/Nairobi")));
 		SpringApplication.run(IMSApplication.class, args);
 	}
 
